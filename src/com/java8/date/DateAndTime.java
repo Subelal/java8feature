b2638993d0d2ecbdc4d7dfa0020bd5632c512450
sramchar <subelal.pal@gmail.com>
2023-09-06 17:22:27 +0530

    new directory for date and time

---
+++ src/com/java8/date/DateAndTime.java
@@ -0,0 +1,45 @@
+package com.java8.date;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class DateAndTime {
+
+    public static void main(String[] args) {
+        List<Person> listOfPerson = new ArrayList<>();
+
+        try(BufferedReader reader =
+                   new BufferedReader(
+                           new InputStreamReader(
+                                   DateAndTime.class
+                                           .getResourceAsStream("people.txt")
+                           )
+                   );
+            Stream<String> stream = reader.lines();
+                ){
+                stream.map(
+                        line ->{
+                   String[] s = line.split(" ");
+                   String name = s[0].trim();
+                   int year = Integer.parseInt(s[1]);
+                            Month month = Month.of(Integer.parseInt(s[2]));
+                            int day = Integer.parseInt(s[3]);
+                            Person p = new Person(name, LocalDate.of(year,month,day));
+                            listOfPerson.add(p);
+                            return p;
+                }).forEach(System.out::println);
+
+        }catch (IOException IOE){
+            System.out.println(" Exception is "+IOE);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+}
